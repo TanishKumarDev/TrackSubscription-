c1cdbee736139a5989a98a993cecb4cf4397ed3e
Tanish Kumar <tanish.kumar.contact@gmail.com>
2025-06-21 10:00:57 +0530

    feat: add database connection and user/subscription models

---
+++ database/connectToDatabase.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { MONGODB_URI, NODE_ENV } from "../config/.env.js";
+
+if (!MONGODB_URI) {
+  throw new Error("❌ MONGODB_URI not found in environment file");
+}
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log(`✅ Connected to MongoDB in ${NODE_ENV} mode`);
+  } catch (error) {
+    console.error("❌ DB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectToDatabase;
